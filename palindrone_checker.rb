aa2b2ebe33269f355dfb7c9c635f5afda30f95e6
Keveren Freeman <freemankeveren@gmail.com>
2017-06-09 10:01:09 -0600

    added recurring loop and exit

--- palindrone_checker.rb
+++ palindrone_checker.rb
@@ -1,21 +1,32 @@
-@arr = []
+arr = []
 
 def palindrone
-  puts "Check to see if it is a palindrone"
-  user_enters
+  puts "Check to see if it is a palindrone!
+        Enter 1 to check or enter 2 to exit."
+  number = gets.chomp.to_i
+  case number
+  when 1
+    user_enters
+  when 2
+    puts "Goodbye."
+  else
+    "Incorrect input try again."
+    palindrone
+  end
 end
 
 def user_enters
-  puts "what word or phrase would you like to check?"
-  user_input = gets.chomp.to_s
-  @arr << user_input
-  @reverse = "#{user_input}".delete(' ').chars.reduce { |x, y| y + x }
-  if @reverse == user_input.delete(' ')
-    puts "this is a palindrone"
+  arr = []
+  puts "What word or phrase would you like to check?"
+  user_input = gets.chomp.to_s.downcase
+  arr << user_input
+  reverse = "#{user_input}".delete(' ').chars.reduce { |x, y| y + x }
+  if reverse == user_input.delete(' ')
+    puts "This is a palindrone!"
+    palindrone
   else
-    puts "this is not a palindrone"
+    puts "This is not a palindrone."
+    palindrone
   end
-
 end
-
 palindrone
